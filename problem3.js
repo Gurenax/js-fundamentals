3f9b1958187b67f0ed08fd5dd7babb1c0365663c
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-20 14:30:59 +1100

    Prettify solution

--- problem3.js
+++ problem3.js
@@ -2,7 +2,7 @@
 Implement your own version of .map
 Define a function that takes a callback and provides the same functionality as the .map function inbuilt into es6. You can do this as a function that extends the array prototype (which takes a callback function as an argument), or more simply as a function that takes an array as an argument, as well as a callback function. */
 
-Array.prototype.map = function ( fn ) {
+Array.prototype.map = function(fn) {
   // Check if array is present
   if (this === null) {
     console.log('Array not defined')
@@ -16,7 +16,7 @@ Array.prototype.map = function ( fn ) {
   // Create a copy of the array
   copy = [...this]
   // Loop through the array
-  for(let i=0; i<copy.length; i++) {
+  for (let i = 0; i < copy.length; i++) {
     // Perform the callback on every value and index of the array
     value = fn(copy[i], i)
     // Copy the return value of the callback into the copy array
@@ -26,8 +26,8 @@ Array.prototype.map = function ( fn ) {
 }
 
 // Test valid
-const arr = [1,2,3]
-const result = arr.map( (val, index) => {
+const arr = [1, 2, 3]
+const result = arr.map((val, index) => {
   console.log(`Original value at index ${index} is`, val)
   return val * 5
 })
@@ -35,7 +35,7 @@ console.log('New array', result)
 
 // Test empty array
 const test = []
-result2 = test.map( val => {
+result2 = test.map(val => {
   return val * 2
 })
 console.log('Empty array', result2)
