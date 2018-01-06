7fe62ffb3e17c49f204d631203ee0289be0c7116
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-01-06 17:46:49 +1100

    Update problem 9 solution with async/await

--- problem9.js
+++ problem9.js
@@ -2,7 +2,9 @@ console.log('Problem 9')
 
 const timeouterPromise = cb => {
   const promise = new Promise( (callThen, callCatch) => {
-    console.time('timeouter')
+    console.time('timeouter1')
+    console.time('timeouter2')
+    console.time('timeouter3')
     if( cb === undefined ) {
       // cb function was not defined so return an error
       callCatch('Callback is undefined')
@@ -14,7 +16,7 @@ const timeouterPromise = cb => {
         console.log("..now passing through to the cb..")
         // return cb
         callThen(cb)
-      }, 6000)      
+      }, 6000)
     }
   })
   return promise
@@ -30,12 +32,12 @@ timeouterPromise()
     console.error(`PROMISE 1 Error: ${error}`)
   })
 console.log('Finished without callback')
-console.timeEnd('timeouter')
+console.timeEnd('timeouter1')
 
 /* With callback */
 timeouterPromise(() => {
   console.log('Finished 9 in the promise')
-  console.timeEnd('timeouter')
+  console.timeEnd('timeouter2')
 })
   .then( data => {
     console.log('PROMISE 2 data:')
@@ -44,6 +46,17 @@ timeouterPromise(() => {
   .catch(error => {
     console.error(`PROMISE 2 Error: ${error}`)
   })
+
+/* Using async/await */
+const timeouterRequest = async() => {
+  data = await timeouterPromise( () => {
+    console.log('Finished 9 in the promise')
+    console.timeEnd('timeouter3')
+  })
+  console.log('PROMISE 3 data:')
+  data()
+}
+timeouterRequest()
 
 // console.log("After timeouter")
 // console.log("..now heading off to other parts of the program!")
